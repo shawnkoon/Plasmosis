81a861a186732e901a11acf050292547a652bd96
shawnkoon <shawn9405@outlook.com>
2016-03-20 16:36:58 -0700

    Added menu bar

--- Plasmosis/Form1.Designer.cs
+++ Plasmosis/Form1.Designer.cs
@@ -28,7 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.selectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.howToToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.symmetricEncryptionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hashingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.otherEncryptionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // selectionToolStripMenuItem
+            // 
+            this.selectionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.symmetricEncryptionToolStripMenuItem,
+            this.hashingToolStripMenuItem,
+            this.otherEncryptionsToolStripMenuItem});
+            this.selectionToolStripMenuItem.Name = "selectionToolStripMenuItem";
+            this.selectionToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.selectionToolStripMenuItem.Text = "Selection";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // howToToolStripMenuItem
+            // 
+            this.howToToolStripMenuItem.Name = "howToToolStripMenuItem";
+            this.howToToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.howToToolStripMenuItem.Text = "How to";
+            this.howToToolStripMenuItem.Click += new System.EventHandler(this.howToToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.selectionToolStripMenuItem,
+            this.aboutToolStripMenuItem,
+            this.howToToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(588, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // symmetricEncryptionToolStripMenuItem
+            // 
+            this.symmetricEncryptionToolStripMenuItem.Name = "symmetricEncryptionToolStripMenuItem";
+            this.symmetricEncryptionToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.symmetricEncryptionToolStripMenuItem.Text = "Symmetric Encryption";
+            // 
+            // hashingToolStripMenuItem
+            // 
+            this.hashingToolStripMenuItem.Name = "hashingToolStripMenuItem";
+            this.hashingToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.hashingToolStripMenuItem.Text = "Hashing";
+            // 
+            // otherEncryptionsToolStripMenuItem
+            // 
+            this.otherEncryptionsToolStripMenuItem.Name = "otherEncryptionsToolStripMenuItem";
+            this.otherEncryptionsToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.otherEncryptionsToolStripMenuItem.Text = "Other Encryptions";
             // 
             // Form1
             // 
@@ -36,17 +98,30 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(588, 468);
+            this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Opacity = 0.95D;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Plasmosis - Make Encryption Great Again   lol jk";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStripMenuItem selectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem howToToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem symmetricEncryptionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hashingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem otherEncryptionsToolStripMenuItem;
     }
 }
 
--- Plasmosis/Form1.cs
+++ Plasmosis/Form1.cs
@@ -16,5 +16,15 @@ namespace Plasmosis
         {
             InitializeComponent();
         }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The Best Plasmosis\nThe Great Plasmosis", "About Us");
+        }
+
+        private void howToToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Just Click Selection on the Menu bar\n\nSelect What you want bro","Hello I am How to");
+        }
     }
 }
