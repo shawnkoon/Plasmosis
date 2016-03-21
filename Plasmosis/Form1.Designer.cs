1353509743ed1ce8205da4c138c4e0a8af99d5b9
cwade12c <cwade12c@gmail.com>
2016-03-21 00:09:20 -0700

    caesarShiftEncrypt
    Added method for caesar cipher encryption. Also updated the menubar and
    added a call to new caesarShiftEncrypt when "How-To" is clicked.

--- Plasmosis/Form1.Designer.cs
+++ Plasmosis/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Plasmosis
 {
-    partial class Form1
+    partial class frmSymmetric
     {
         /// <summary>
         /// Required designer variable.
@@ -29,12 +29,13 @@
         private void InitializeComponent()
         {
             this.selectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.howToToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.symmetricEncryptionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.hashingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.otherEncryptionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.howToToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -47,32 +48,6 @@
             this.selectionToolStripMenuItem.Name = "selectionToolStripMenuItem";
             this.selectionToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
             this.selectionToolStripMenuItem.Text = "Selection";
-            // 
-            // aboutToolStripMenuItem
-            // 
-            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
-            this.aboutToolStripMenuItem.Text = "About";
-            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
-            // 
-            // howToToolStripMenuItem
-            // 
-            this.howToToolStripMenuItem.Name = "howToToolStripMenuItem";
-            this.howToToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
-            this.howToToolStripMenuItem.Text = "How to";
-            this.howToToolStripMenuItem.Click += new System.EventHandler(this.howToToolStripMenuItem_Click);
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.selectionToolStripMenuItem,
-            this.aboutToolStripMenuItem,
-            this.howToToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(588, 24);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
             // 
             // symmetricEncryptionToolStripMenuItem
             // 
@@ -90,9 +65,43 @@
             // 
             this.otherEncryptionsToolStripMenuItem.Name = "otherEncryptionsToolStripMenuItem";
             this.otherEncryptionsToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
-            this.otherEncryptionsToolStripMenuItem.Text = "Other Encryptions";
+            this.otherEncryptionsToolStripMenuItem.Text = "Other";
             // 
-            // Form1
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.selectionToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(588, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.howToToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // howToToolStripMenuItem
+            // 
+            this.howToToolStripMenuItem.Name = "howToToolStripMenuItem";
+            this.howToToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.howToToolStripMenuItem.Text = "How-To";
+            this.howToToolStripMenuItem.Click += new System.EventHandler(this.howToToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // frmSymmetric
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -102,7 +111,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
-            this.Name = "Form1";
+            this.Name = "frmSymmetric";
             this.Opacity = 0.95D;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Plasmosis - Make Encryption Great Again   lol jk";
@@ -116,12 +125,13 @@
         #endregion
 
         private System.Windows.Forms.ToolStripMenuItem selectionToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem howToToolStripMenuItem;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem symmetricEncryptionToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem hashingToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem otherEncryptionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem howToToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
     }
 }
 
--- Plasmosis/Form1.cs
+++ Plasmosis/Form1.cs
@@ -10,21 +10,63 @@ using System.Windows.Forms;
 
 namespace Plasmosis
 {
-    public partial class Form1 : Form
+    public partial class frmSymmetric : Form
     {
-        public Form1()
+        public frmSymmetric()
         {
             InitializeComponent();
         }
 
-        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("The Best Plasmosis\nThe Great Plasmosis", "About Us");
-        }
+
 
         private void howToToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Just Click Selection on the Menu bar\n\nSelect What you want bro","Hello I am How to");
+            MessageBox.Show(caesarShiftEncrypt("abc", 3),"Hello I am How to");
+        }
+
+        private String caesarShiftEncrypt(String str, int shift)
+        {
+            char[] charset = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+                              'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+            String result = "";
+
+            foreach(char c in str.ToCharArray())
+            {
+                int j = 0;
+
+                for(int i = 0; i < charset.Length; i++)
+                {
+                    if(c.Equals(charset[i]))
+                    {
+                        j = i;
+                        i = charset.Length;
+                    }
+                }
+
+                j = (j + shift) % 26;
+
+                for(int i = 0; i < charset.Length; i++)
+                {
+                    if(i == j)
+                    {
+                        result += charset[i];
+                        i = charset.Length;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private void aboutToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
